84065464976c1ccbc8226a42bf0304b59cf82624
Yilmaz <yilmazbedirhan35@gmail.com>
2022-10-29 21:02:19 +0300

    signup connection

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -52,7 +52,26 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
-    fetch("");
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
     auth.login();
   };
