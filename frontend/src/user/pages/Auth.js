28bd27d00f3145dec476ef4c92e1e8e5e7efd895
Yilmaz <yilmazbedirhan35@gmail.com>
2022-10-29 22:43:59 +0300

    getting users

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -55,10 +55,34 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setError(err.message || "Error! Try again!");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -76,7 +100,6 @@ const Auth = () => {
           throw new Error(responseData.message);
         }
 
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
