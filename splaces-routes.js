6a929a887e2c7f377353b9385dc0d6bd425323fb
Yilmaz <yilmazbedirhan35@gmail.com>
2022-11-01 17:33:12 +0300

    Route Protection with Auth

--- splaces-routes.js
+++ splaces-routes.js
@@ -17,16 +17,25 @@ router.post(
   '/',
   fileUpload.single('image'),
   [
-    check('title').not().isEmpty(),
+    check('title')
+      .not()
+      .isEmpty(),
     check('description').isLength({ min: 5 }),
-    check('address').not().isEmpty()
+    check('address')
+      .not()
+      .isEmpty()
   ],
   placesControllers.createPlace
 );
 
 router.patch(
   '/:pid',
-  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
   placesControllers.updatePlace
 );
 
